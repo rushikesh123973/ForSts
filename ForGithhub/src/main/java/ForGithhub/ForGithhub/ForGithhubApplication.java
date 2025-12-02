7667489f5aca7b95b9f4398e9f5047981dd3e147
Aarav Computer <Aarav Computer@DESKTOP-Q6K5KJ5>
2025-12-02 08:16:58 +0530

    first Commit

---
+++ ForGithhub/src/main/java/ForGithhub/ForGithhub/ForGithhubApplication.java
@@ -0,0 +1,13 @@
+package ForGithhub.ForGithhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForGithhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForGithhubApplication.class, args);
+	}
+
+}
---
+++ ForGithhub/src/test/java/ForGithhub/ForGithhub/ForGithhubApplicationTests.java
@@ -0,0 +1,13 @@
+package ForGithhub.ForGithhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForGithhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
